4a48fcbc57be5fe863c270adec676accd0bc1876
Simon <sbridge67@gmail.com>
2017-12-05 21:11:57 +0000

    initial

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body {
+  background: linear-gradient(150deg, #0ab44d 10%, #09b3af 70%, #09b3af 94%);
+  display: block;
+}
+html {
+  height: 100%;
+}
+</style>
